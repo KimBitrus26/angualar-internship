3f374afc280f4d3b0482b1bf556fc2aa6d32c729
KimBitrus26 <bitruschoji4real@gmail.com>
2020-08-31 21:11:17 +0100

    commit

--- Desktop/djangojobtask/screening_app/sreening/admin.py
+++ Desktop/djangojobtask/screening_app/sreening/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from . models import Question, Story
-from django.contrib.auth.models import User
+
 # Register your models here.
 
 
@@ -15,13 +15,8 @@ class StoryAdmin(admin.ModelAdmin):
 
     pass
 
-class UserAdmin(admin.ModelAdmin):
-
-    pass
-
-
 admin.site.register(Question, QuestionAdmin)
 
 admin.site.register(Story, StoryAdmin)
-admin.site.register(User, UserAdmin)
+
 
